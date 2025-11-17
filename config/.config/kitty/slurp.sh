84295141a213444f184c0a9ed9ad28c7a8b2fdd2
tr1x_em <admin@trix.is-a.dev>
2025-11-17 16:36:48 +0530

    Automated Dev Commit : 2025-11-17 16:36:40

--- config/.config/kitty/slurp.sh
+++ config/.config/kitty/slurp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # execute slurp with -f "%x %y %w %h" and set variables
-read -r X Y W H < <(slurp -f "%x %y %w %h" -b 000000bf -c ffb4ab)
+read -r X Y W H < <(slurp -f "%x %y %w %h" -b 000000bf -c ffb59a)
 # execute kitty with override and disown
 hyprctl dispatch -- exec "[move $X $Y]kitty -o initial_window_width="$W" -o initial_window_height="$H" --class kitty-floating -1"
 
