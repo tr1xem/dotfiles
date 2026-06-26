8cfbefd38e8da608e3404accd74ea785bfd6e4ec
tr1xem <admin@trix.is-a.dev>
2026-06-26 21:10:06 +0530

    misc tweaks

--- config/.config/xmobar/scripts/weather.sh
+++ config/.config/xmobar/scripts/weather.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
 PRIMARY="$1"
-weather=$(curl -s "wttr.in?format=%c%t" | sed 's/+//' | xargs)
-echo "<fc=$PRIMARY>$weather</fc>"
+
+weather=$(curl -s \
+    --retry 3 \
+    --retry-delay 3 \
+    --max-time 3 \
+    "wttr.in?format=%c%t" 2>/dev/null)
+
+if [[ -n "$weather" ]]; then
+    echo "<fc=$PRIMARY>$weather</fc>"
+else
+    echo ""
+fi
